92eac5cd1a843d683629a45530664fec1f587a9c
enguillem <enguillem@gmail.com>
2024-06-13 12:48:21 +0200

    endpoint para borrar usuarios

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -65,3 +65,17 @@ def update_user_role(user_id: int, role_id: int, db: Session = Depends(get_db)):
 def list_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
+
+
+@router.delete("/{user_id}", response_model=UserSchema)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Cargar la relación de rol antes de eliminar el usuario
+    db_user.role  # Acceder a la relación para cargarla
+
+    db.delete(db_user)
+    db.commit()
+    return db_user
